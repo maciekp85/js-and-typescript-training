ba0815c8b07ff38c48f870cad6dfc8005223e568
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2023-04-26 21:16:47 +0200

    MDN: Events example

--- web-projects/test-site/scripts/main.js
+++ web-projects/test-site/scripts/main.js
@@ -1,2 +1,11 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!"
+
+document.querySelector("html").addEventListener("click", function () {
+    alert("Ouch! Stop poking me!");
+})
+
+// Alternative way of writing anonymous function
+document.querySelector("html").addEventListener("click", () => {
+    alert("Ouch! Stop poking me. Alternative way.")
+})
