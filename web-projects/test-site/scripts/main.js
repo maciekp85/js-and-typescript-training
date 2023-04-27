f22b1f7dd00160c2ee7e09900f4a75585cb3c7e0
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2023-04-27 20:13:17 +0200

    MDN: Supercharging our example website

--- web-projects/test-site/scripts/main.js
+++ web-projects/test-site/scripts/main.js
@@ -1,11 +1,34 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!"
+const myImage = document.querySelector("img");
 
-document.querySelector("html").addEventListener("click", function () {
-    alert("Ouch! Stop poking me!");
-})
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/firefox-icon.png") {
+        myImage.setAttribute("src", "images/firefox.jpeg");
+    } else {
+        myImage.setAttribute("src", "images/firefox-icon.png");
+    }
+}
 
-// Alternative way of writing anonymous function
-document.querySelector("html").addEventListener("click", () => {
-    alert("Ouch! Stop poking me. Alternative way.")
-})
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = () => {
+    setUserName();
+}
