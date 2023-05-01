d84f069cb207dee36e1faa11e034fdc8d5c1b76b
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2023-05-01 20:03:10 +0200

    MDN: Comments

--- web-projects/javascript/script.js
+++ web-projects/javascript/script.js
@@ -1,8 +1,16 @@
+// Function: creates a new paragraph and appends it to the bottom of the HTML body.
 function createParagraph() {
     const para = document.createElement("p");
     para.textContent = "You clicked the button!";
     document.body.appendChild(para);
-  }
+}
+  
+/*
+  1. Get references to all the buttons on the page in an array format.
+  2. Loop through all the buttons and add a click event listener to each one.
+
+  When any button is pressed, the createParagraph() function will be run.
+*/
   
   const buttons = document.querySelectorAll("button");
   
