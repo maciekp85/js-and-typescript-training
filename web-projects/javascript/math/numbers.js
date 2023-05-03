c271c4639c81943dd1115124a4411eae051b8970
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2023-05-03 17:42:50 +0200

    MDN: Useful Number methods

--- web-projects/javascript/math/numbers.js
+++ web-projects/javascript/math/numbers.js
@@ -1,7 +1,15 @@
+// It's all numbers to me
+
 const myInt = 5;
 const myFloat = 6.667;
-myInt;
-myFloat;
+console.log("myInt = ", myInt);
+console.log("myFloat = ", myFloat);
 
 console.log("typeof myInt: ", typeof myInt);
 console.log("typeof myFloat: ", typeof myFloat);
+
+// Useful Number methods
+const lotsOfDecimal = 1.766584958675746364;
+console.log("lotsOfDecimal = ", lotsOfDecimal);
+const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
+console.log('lotsOfDecimal rounded to 2 decimal places = ', twoDecimalPlaces);
