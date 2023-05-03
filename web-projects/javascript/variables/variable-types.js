66863235022afedc4bc670cadb86fcc2bba104d4
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2023-05-03 14:33:10 +0200

    MDN: Dynamic typing

--- web-projects/javascript/variables/variable-types.js
+++ web-projects/javascript/variables/variable-types.js
@@ -15,11 +15,19 @@ let myNameArray = ['Chris', 'Bob', 'Jim'];
 let myNumberArray = [10, 15, 40];
 
 myNameArray[0]; // should return 'Chris'
-console.log(myNameArray[0]);
+console.log('First element from array: ', myNameArray[0]);
 myNumberArray[2]; // should return 40
-console.log(myNameArray[2]);
+console.log('Second element from array: ', myNameArray[2]);
 
 // Objects
 let dog = { name: 'Spot', breed: 'Dalmation' };
 
-console.log(dog.name);
+console.log('Dog name: ', dog.name);
+
+// Dynamic typing
+let myString = 'Hello';
+
+let myNumber = '500';   // oops, this is still a string
+console.log('typeof myNumber: ', typeof myNumber);
+myNumber = 500; // much better - now this is a number
+console.log('typeof myNumber: ', typeof myNumber);
