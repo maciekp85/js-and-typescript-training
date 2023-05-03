01e6c84f99dae59d34bbf8bcf0976872af1c74d9
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2023-05-03 14:25:45 +0200

    MDN: Variable types

---
+++ web-projects/javascript/variables/variable-types.js
@@ -0,0 +1,25 @@
+// Variable types
+
+// Numbers
+let myAge = 17;
+
+// Strings
+let dolphinGoodbye = 'So long and thanks for all the fish';
+
+// Booleans
+let iAmAlive = true;
+let test = 6 < 3;
+
+// Arrays
+let myNameArray = ['Chris', 'Bob', 'Jim'];
+let myNumberArray = [10, 15, 40];
+
+myNameArray[0]; // should return 'Chris'
+console.log(myNameArray[0]);
+myNumberArray[2]; // should return 40
+console.log(myNameArray[2]);
+
+// Objects
+let dog = { name: 'Spot', breed: 'Dalmation' };
+
+console.log(dog.name);
