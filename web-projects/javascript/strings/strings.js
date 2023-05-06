f3d79c9ca2be254a0cf2282e08bcfa31b2b5654a
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2023-05-06 20:39:02 +0200

    MDN: Creating a string

---
+++ web-projects/javascript/strings/strings.js
@@ -0,0 +1,7 @@
+// Creating a string
+console.log("\n---Creating a string---");
+const string = "The revolution will not be televised.";
+console.log(string);
+
+const badString = string;
+console.log(badString);
