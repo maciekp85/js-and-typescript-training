5312c10915717303c2f97cbdb7cdbaebf6b03eb5
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2023-05-14 19:29:20 +0200

    MDN: Extracting a substring from a string

--- web-projects/javascript/strings/useful_string_methods.js
+++ web-projects/javascript/strings/useful_string_methods.js
@@ -1,6 +1,7 @@
+const browserType = "mozilla";
+
 // Finding the length of a string
 console.log("\n---Finding the length of a string---");
-const browserType = "mozilla";
 console.log("text: " + browserType);
 console.log("Length: " + browserType.length);
 
@@ -45,3 +46,14 @@ const secondOccurence = tagline.indexOf("developers", firstOccurrence + 1);
 
 console.log("First occurence index: ", firstOccurrence); // 20
 console.log("Second occurence index: ", secondOccurence); // 35 Here we're telling the method to search for the substring "developers" starting at index 21 (firstOccurrence + 1), and it returns the index 35.
+
+// Extracting a substring from a string
+console.log("\n---Extracting a substring from a string---");
+// You can extract a substring from a string using the slice() method. You pass it:
+// the index at which to start extracting
+// the index at which to stop extracting. This is exclusive, meaning that the character at this index is not included in the extracted substring.
+
+console.log(browserType.slice(1, 4)); // The character at index 1 is "o", and the character at index 4 is "l".
+// So we extract all characters starting at "o" and ending just before "l", giving us "ozi".
+
+console.log(browserType.slice(2)); // zilla
