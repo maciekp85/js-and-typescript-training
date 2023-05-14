ac1d8f6c7098db6cd71304e74e2ce6d4c6cb467d
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2023-05-14 12:36:47 +0200

    MDN: Retrieving a specific string character

--- web-projects/javascript/strings/useful_string_methods.js
+++ web-projects/javascript/strings/useful_string_methods.js
@@ -1,4 +1,11 @@
 // Finding the length of a string
 console.log("\n---Finding the length of a string---");
 const browserType = "mozilla";
-console.log(browserType.length);
+console.log("text: " + browserType);
+console.log("Length: " + browserType.length);
+
+// Retrieving a specific string character
+console.log("\n---Retrieving a specific string character---");
+firstCharacter = browserType[0];
+console.log("First character: " + firstCharacter);
+console.log("Last character: " + browserType[browserType.length - 1]);
