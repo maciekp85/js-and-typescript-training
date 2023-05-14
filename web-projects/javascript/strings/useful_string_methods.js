f4059edb2e11a1c549f42dfd3925da7ee52792b9
Maciej Piotrowski <maciejpiotrowski85@hotmail.com>
2023-05-14 19:05:55 +0200

    MDN: Testing if a string contains a substring

--- web-projects/javascript/strings/useful_string_methods.js
+++ web-projects/javascript/strings/useful_string_methods.js
@@ -9,3 +9,27 @@ console.log("\n---Retrieving a specific string character---");
 firstCharacter = browserType[0];
 console.log("First character: " + firstCharacter);
 console.log("Last character: " + browserType[browserType.length - 1]);
+
+// Testing if a string contains a substring
+console.log("\n---Testing if a string contains a substring---");
+
+console.log("includes() method");
+if (browserType.includes("zilla")) {
+    console.log("Found zilla");
+} else {
+    console.log("No zilla here!");
+}
+
+console.log("startsWith() method");
+if (browserType.startsWith("zilla")) {
+    console.log("Found zilla");
+} else {
+    console.log("No zilla here!");
+}
+
+console.log("endsWith() method");
+if (browserType.endsWith("zilla")) {
+    console.log("Found zilla");
+} else {
+    console.log("No zilla here!");
+}
